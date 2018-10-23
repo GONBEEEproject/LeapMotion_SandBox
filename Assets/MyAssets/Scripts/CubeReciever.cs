9340a4d4e4730576e30583e9b7179dd1991c3b9c
Rikuya Fukuda\GONBEEE_project <gonbeee.project@gmail.com>
2018-10-23 11:53:23 +0900

    First Commit

---
+++ Assets/MyAssets/Scripts/CubeReciever.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeReciever : MonoBehaviour {
+
+    private MeshRenderer renderer;
+
+    public void Activate()
+    {
+        renderer.enabled = true;
+    }
+
+    public void Inactive()
+    {
+        renderer.enabled = false;
+    }
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
